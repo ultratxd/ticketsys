0361c7444c60e60a028f68828f30203658c0880d
wangliwei <wangliwei40128@163.com>
2019-11-11 13:46:52 +0800

    同步订单数据——新增

--- src/main/kotlin/com/cj/ticketsys/controller/clientapi/ApiController.kt
+++ src/main/kotlin/com/cj/ticketsys/controller/clientapi/ApiController.kt
@@ -6,8 +6,11 @@ import com.cj.ticketsys.controller.dto.Result
 import com.cj.ticketsys.dao.ClientDataDao
 import com.cj.ticketsys.entities.ClientGateLog
 import com.cj.ticketsys.entities.ClientOrder
+import com.cj.ticketsys.entities.ClientSubOrder
+import com.cj.ticketsys.entities.SubOrder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
+import java.util.*
 import javax.servlet.http.HttpServletRequest
 
 @RestController
@@ -47,16 +50,62 @@ class ApiController {
     fun order(
             @RequestBody orderBody: OrderReqBody,
             req: HttpServletRequest
-    ) {
+    ): Result  {
         val order = ClientOrder()
+        order.clientId = orderBody.clientId;
+        order.cloudId = orderBody.cloudId;
+        order.clientOrderNo = orderBody.clientOrderNo;
+        order.nums = orderBody.nums;
+        order.orderType = orderBody.orderType;
+        order.amount = orderBody.amount;
+        order.perNums = orderBody.perNums;
+        order.createTime = orderBody.createTime;
+        order.state = orderBody.state;
+        order.payType = orderBody.payType;
+        order.realPay = orderBody.realPay;
+        order.changePay = orderBody.changePay;
+        order.shouldPay = orderBody.shouldPay;
+        order.exCode = orderBody.exCode;
+        order.remark = orderBody.remark;
+        order.saleClientNo = orderBody.saleClientNo;
+        order.ext1 = orderBody.ext1;
+        order.ext2 = orderBody.ext2;
+        order.ext3 = orderBody.ext3;
 
+        val c = dataDao.insertOrder(order)
+        if(c > 0) {
+            return Result(RESULT_SUCCESS,"ok")
+        }
+        return Result(RESULT_FAIL,"fail")
     }
 
     @PostMapping("/sub_order")
     fun subOrder(
-            @RequestBody order: OrderReqBody,
+            @RequestBody subOrderBody: SubOrderReqBody,
             req: HttpServletRequest
-    ) {
+    ): Result  {
+        val subOrder = ClientSubOrder();
+        subOrderBody.clientId = subOrder.clientId;
+        subOrderBody.cloudId = subOrder.cloudId;
+        subOrderBody.clientOrderNo = subOrder.clientOrderNo;
+        subOrderBody.orderType = subOrder.orderType;
+        subOrderBody.ticketId = subOrder.ticketId;
+        subOrderBody.ticketName = subOrder.ticketName;
+        subOrderBody.amount = subOrder.amount;
+        subOrderBody.unitPrice = subOrder.unitPrice;
+        subOrderBody.nums = subOrder.nums;
+        subOrderBody.perNums = subOrder.perNums;
+        subOrderBody.createTime = subOrder.createTime;
+        subOrderBody.prints = subOrder.prints;
+        subOrderBody.useDate = subOrder.useDate;
+        subOrderBody.enterTime = subOrder.enterTime;
 
+        val c = dataDao.insertSubOrder(subOrder);
+        if(c > 0) {
+            return Result(RESULT_SUCCESS,"ok")
+        }
+        return Result(RESULT_FAIL,"fail")
     }
+
+
 }
